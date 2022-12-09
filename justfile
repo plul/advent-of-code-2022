f0c3269502194a9db506b1cbc19e0d13ff0303b4
Asger Juul Brunshøj <asgerbrunshoj@gmail.com>
2022-12-10 00:15:27 +0100

    feat: benchmarking

--- justfile
+++ justfile
@@ -1,14 +1,45 @@
 # Check, test, lint, then run
-default:
-	cargo check
-	cargo test
+default: _build _build-release
+	cargo nextest run
 	cargo clippy --tests -- -D rust-2018-idioms
 	cargo +nightly fmt --check
 	cargo run
+	cargo build --release
+	hyperfine --shell=none --warmup 5 './target/release/advent-of-code-2022'
 
 # List recipes
-list: 
+list:
 	@just --list
+
+# Run and benchmark
+run +ARGS: _build _build-release
+	cargo run -- {{ARGS}}
+	hyperfine --shell=none --warmup 5 './target/release/advent-of-code-2022 {{ARGS}}'
+
+# Benchmark each problem individually
+bench-each:
+	just bench 1 1
+	just bench 1 2
+	just bench 2 1
+	just bench 2 2
+	just bench 3 1
+	just bench 3 2
+	just bench 4 1
+	just bench 4 2
+	just bench 5 1
+	just bench 5 2
+	just bench 6 1
+	just bench 6 2
+	just bench 7 1
+	just bench 7 2
+	just bench 8 1
+	just bench 8 2
+	just bench 9 1
+	just bench 9 2
+
+# Benchmark a given problem (or all together if none specified)
+bench *ARGS: _build-release
+	hyperfine --shell=none --warmup 5 './target/release/advent-of-code-2022 {{ARGS}}'
 
 # Edit this justfile
 edit:
@@ -17,7 +48,13 @@ edit:
 # Format code
 fmt:
 	cargo +nightly fmt
-	
+
 # Open docs
 doc:
 	cargo doc --open
+
+_build:
+	cargo build
+
+_build-release:
+	cargo build --release
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![feature(binary_heap_into_iter_sorted)]
 #![feature(array_chunks)]
 
+use clap::Parser;
 use std::path::Path;
+use std::time::Instant;
 
 mod day_01;
 mod day_02;
@@ -13,25 +15,72 @@ mod day_07;
 mod day_08;
 mod day_09;
 
+#[derive(Parser, Debug)]
+struct Cli {
+    day: Option<usize>,
+    part: Option<usize>,
+}
+
 fn main() {
-    println!("Day 1 Part 1: {}", day_01::part_1(&read_input("day_01.txt")));
-    println!("Day 1 Part 2: {}", day_01::part_2(&read_input("day_01.txt")));
-    println!("Day 2 Part 1: {}", day_02::part_1(&read_input("day_02.txt")));
-    println!("Day 2 Part 2: {}", day_02::part_2(&read_input("day_02.txt")));
-    println!("Day 3 Part 1: {}", day_03::part_1(&read_input("day_03.txt")));
-    println!("Day 3 Part 2: {}", day_03::part_2(&read_input("day_03.txt")));
-    println!("Day 4 Part 1: {}", day_04::part_1(&read_input("day_04.txt")));
-    println!("Day 4 Part 2: {}", day_04::part_2(&read_input("day_04.txt")));
-    println!("Day 5 Part 1: {}", day_05::part_1(&read_input("day_05.txt")));
-    println!("Day 5 Part 2: {}", day_05::part_2(&read_input("day_05.txt")));
-    println!("Day 6 Part 1: {}", day_06::part_1(&read_input("day_06.txt")));
-    println!("Day 6 Part 2: {}", day_06::part_2(&read_input("day_06.txt")));
-    println!("Day 7 Part 1: {}", day_07::part_1(&read_input("day_07.txt")));
-    println!("Day 7 Part 2: {}", day_07::part_2(&read_input("day_07.txt")));
-    println!("Day 8 Part 1: {}", day_08::part_1(&read_input("day_08.txt")));
-    println!("Day 8 Part 2: {}", day_08::part_2(&read_input("day_08.txt")));
-    println!("Day 9 Part 1: {}", day_09::part_1(&read_input("day_09.txt")));
-    println!("Day 9 Part 2: {}", day_09::part_2(&read_input("day_09.txt")));
+    let cli = Cli::parse();
+
+    if let Some(day) = cli.day {
+        if let Some(part) = cli.part {
+            solve(day, part);
+        } else {
+            solve(day, 1);
+            solve(day, 2);
+        }
+    } else {
+        solve(1, 1);
+        solve(1, 2);
+        solve(2, 1);
+        solve(2, 2);
+        solve(3, 1);
+        solve(3, 2);
+        solve(4, 1);
+        solve(4, 2);
+        solve(5, 1);
+        solve(5, 2);
+        solve(6, 1);
+        solve(6, 2);
+        solve(7, 1);
+        solve(7, 2);
+        solve(8, 1);
+        solve(8, 2);
+        solve(9, 1);
+        solve(9, 2);
+    }
+}
+
+fn solve(day: usize, part: usize) {
+    let input = read_input(format!("day_{day:02}.txt"));
+
+    let now = Instant::now();
+    let solution = match (day, part) {
+        (1, 1) => day_01::part_1(&input).to_string(),
+        (1, 2) => day_01::part_2(&input).to_string(),
+        (2, 1) => day_02::part_1(&input).to_string(),
+        (2, 2) => day_02::part_2(&input).to_string(),
+        (3, 1) => day_03::part_1(&input).to_string(),
+        (3, 2) => day_03::part_2(&input).to_string(),
+        (4, 1) => day_04::part_1(&input).to_string(),
+        (4, 2) => day_04::part_2(&input).to_string(),
+        (5, 1) => day_05::part_1(&input),
+        (5, 2) => day_05::part_2(&input),
+        (6, 1) => day_06::part_1(&input).to_string(),
+        (6, 2) => day_06::part_2(&input).to_string(),
+        (7, 1) => day_07::part_1(&input).to_string(),
+        (7, 2) => day_07::part_2(&input).to_string(),
+        (8, 1) => day_08::part_1(&input).to_string(),
+        (8, 2) => day_08::part_2(&input).to_string(),
+        (9, 1) => day_09::part_1(&input).to_string(),
+        (9, 2) => day_09::part_2(&input).to_string(),
+        _ => panic!(),
+    };
+    let elapsed = now.elapsed();
+
+    println!("{:10}μs   Day {day} Part {part}: {solution}", elapsed.as_micros());
 }
 
 fn read_input(path: impl AsRef<Path>) -> String {
