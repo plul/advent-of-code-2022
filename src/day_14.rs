359dfcb9e316e11f7b1c8ff9fd2c3e43da69cca1
Asger Juul Brunshøj <asgerbrunshoj@gmail.com>
2022-12-14 10:38:19 +0100

    feat: day 14 part 2

--- src/day_14.rs
+++ src/day_14.rs
@@ -59,8 +59,64 @@ pub fn part_1(input: &str) -> usize {
 }
 
 pub fn part_2(input: &str) -> usize {
-    parser::parse(input);
-    Default::default()
+    let paths = parser::parse(input);
+
+    // Find largest y value of rock so we know when a corn of sand would be falling endlessly.
+    let max_y_of_rock = paths.iter().flat_map(|p| p.iter()).map(|v| v.y).max().unwrap();
+
+    let floor = max_y_of_rock + 2;
+
+    // Build world
+    let mut world: HashMap<Vector2D<i64>, Element> = HashMap::new();
+    for path in paths {
+        let mut path_iter = path.into_iter();
+        let mut current_point = path_iter.next().unwrap();
+        world.insert(current_point, Element::Rock);
+        for next_point_in_path in path_iter {
+            while current_point != next_point_in_path {
+                let diff = next_point_in_path - current_point;
+                let step = diff.clamp_x(-1, 1).clamp_y(-1, 1);
+                current_point += step;
+                world.insert(current_point, Element::Rock);
+            }
+        }
+    }
+
+    // Simulate falling sand
+    let spawn_point: Vector2D<i64> = Vector2D::from((500, 0));
+    let mut units_of_sand_come_to_rest = 0;
+    loop {
+        if world.contains_key(&spawn_point) {
+            // End of simulation
+            break;
+        }
+
+        let mut current_point = spawn_point;
+
+        loop {
+            if current_point.y + 1 >= floor {
+                // Deposit here.
+                world.insert(current_point, Element::Sand);
+                units_of_sand_come_to_rest += 1;
+                break;
+            }
+
+            if !world.contains_key(&(current_point + (0, 1))) {
+                current_point += (0, 1);
+            } else if !world.contains_key(&(current_point + (-1, 1))) {
+                current_point += (-1, 1);
+            } else if !world.contains_key(&(current_point + (1, 1))) {
+                current_point += (1, 1);
+            } else {
+                // Deposit here.
+                world.insert(current_point, Element::Sand);
+                units_of_sand_come_to_rest += 1;
+                break;
+            }
+        }
+    }
+
+    units_of_sand_come_to_rest
 }
 
 type PathOfRock = Vec<Vector2D<i64>>;
@@ -99,7 +155,7 @@ fn part_1_example() {
     assert_eq!(part_1(EXAMPLE), 24);
 }
 
-// #[test]
-// fn part_2_example() {
-//     assert_eq!(part_2(EXAMPLE), 0);
-// }
+#[test]
+fn part_2_example() {
+    assert_eq!(part_2(EXAMPLE), 93);
+}
